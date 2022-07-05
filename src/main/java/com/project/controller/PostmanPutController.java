675263665837e1766dd1095332b1d103f1eb78a5
Emerson Millard <emillard.code@gmail.com>
2022-07-05 02:52:05 -0700

    Added controller method.

--- src/main/java/com/project/controller/PostmanPutController.java
+++ src/main/java/com/project/controller/PostmanPutController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @RestController
