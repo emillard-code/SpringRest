fb00f03f9aa631d6055b0d77712c8108bd7d070c
Emerson Millard <emillard.code@gmail.com>
2022-07-05 02:05:23 -0700

    Added controller methods.

--- src/main/java/com/project/controller/PostmanDeleteController.java
+++ src/main/java/com/project/controller/PostmanDeleteController.java
@@ -7,15 +7,30 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public class PostmanDeleteController {
 
     @Autowired
     CoderRepository repository;
 
-    @DeleteMapping("/coder/{id}")
-    public Coder deleteCoderById(@PathVariable("id") int id) {
+    @DeleteMapping("/deleteCoder1/{id}")
+    public Coder deleteCoderById1(@PathVariable("id") int id) {
 
+        Optional<Coder> coder = repository.findById(id);
+        repository.deleteById(id);
+
+        return new Coder();
+
+    }
+
+    @DeleteMapping("/deleteCoder2/{id}")
+    public Coder deleteCoderById2(@PathVariable("id") int id) {
+
+        // Main differences between .findById() and .getOne() is that
+        // the former is eagerly loaded while the latter is lazily loaded.
+        // Technically speaking .getOne() does not even hit the database, just a proxy value.
         Coder coder = repository.getOne(id);
         repository.delete(coder);
 
