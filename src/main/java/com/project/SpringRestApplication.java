667714dff234c0ee35f018f165df13d759f8dd57
Emerson Millard <emillard.code@gmail.com>
2022-07-04 23:50:19 -0700

    Minor comment changes.

--- src/main/java/com/project/SpringRestApplication.java
+++ src/main/java/com/project/SpringRestApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringRestApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringRestApplication.class, args);
+
     }
 
 }
